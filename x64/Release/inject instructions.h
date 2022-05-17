9e291ed19231da5316cdbe25fb7d7e62a9fe88ae
pastor-ritz <104474891+pastor-ritz@users.noreply.github.com>
2022-05-17 06:54:32 -0500

    Create inject instructions.h

---
+++ x64/Release/inject instructions.h
@@ -0,0 +1,5 @@
+1. turn of anti virus and windows defender 
+2. open load injector.exe
+3. f1 to load driver (before u launch fortnite)
+4. make sure ur dll is in the same folder and named ritz.dll
+5. click f2 in lobby to inject
